d1cdcb96b51aee22174f68961e8f1ca29b2319f2
cw-ozaki <log@logn.in>
2019-11-15 22:40:22 +0900

    ..

--- check/entrypoint.sh
+++ check/entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-exec /usr/local/bin/pr check "${GITHUB_REPOSITORY}" "$@"
+if [ "$PR_EXIT_CODE" == "true" ]; then
+  args=" --exit-code"
+fi
+
+if [ "$PR_NO_EXIT_CODE" == "true" ]; then
+  args=" --no-exit-code"
+fi
+
+if [ -n "$PR_RATE" ]; then
+  args=" --rate $PR_RATE"
+fi
+
+exec /usr/local/bin/pr "$args" check "${GITHUB_REPOSITORY}" "$@"
 
