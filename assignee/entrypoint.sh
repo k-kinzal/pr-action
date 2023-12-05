e4f0a5e80f438e868a1a0a900e68ea9233775d4c
k-kinzal <log@logn.in>
2023-12-05 12:05:42 +0900

    support assignee, label, review commands

---
+++ assignee/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "${PR_EXIT_CODE}" = "true" ]; then
+  args="${args} --exit-code"
+fi
+
+if [ "${PR_NO_EXIT_CODE}" = "true" ]; then
+  args="${args} --no-exit-code"
+fi
+
+if [ -n "${PR_RATE}" ]; then
+  args="${args} --rate $PR_RATE"
+fi
+
+exec /usr/local/bin/pr ${args} asignee "${GITHUB_REPOSITORY}" "$@"
